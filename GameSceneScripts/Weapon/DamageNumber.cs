3f60d323e54631bee447d415f2450caad8f49d2b
MinwangLou <minwang.lou@alumnos.upm.es>
2025-06-18 22:19:47 +0200

    Update the animation of text spawner about damage taked by enemy

--- GameSceneScripts/Weapon/DamageNumber.cs
+++ GameSceneScripts/Weapon/DamageNumber.cs
@@ -13,10 +13,37 @@ public class DamageNumber : MonoBehaviour
     private float moveSpeed = 0.5f;
 
 
+    private Vector3 initialScale;
+    private Color initialColor;
+
+
     // Update is called once per frame
     void Update()
     {
         UpdateDamageNumber();
+    }
+
+
+    public void SetUp(int damageDisplay)
+    {
+        numberSpawnCounter = numberSpawnTime;
+        damageText.text = damageDisplay.ToString();
+
+        if (damageDisplay <= 30)
+        {
+            damageText.color = Color.white;
+        }
+        else
+        {
+            damageText.color = Color.red;
+        }
+
+        // 初始化缩放
+        initialScale = transform.localScale = Vector3.one * 1.5f; // 初始放大一点点
+
+        // 初始化颜色
+        initialColor = damageText.color;
+        damageText.color = new Color(initialColor.r, initialColor.g, initialColor.b, 1f);
     }
 
     private void UpdateDamageNumber()
@@ -25,21 +52,25 @@ public class DamageNumber : MonoBehaviour
         {
             numberSpawnCounter -= Time.deltaTime;
 
+            float t = 1 - (numberSpawnCounter / numberSpawnTime); // [0, 1] 之间
+
+            // 渐隐效果
+            Color fadeColor = damageText.color;
+            fadeColor.a = Mathf.Lerp(1f, 0f, t); // 透明度从1渐变到0
+            damageText.color = fadeColor;
+
+            // 缩放回弹
+            float scale = Mathf.Lerp(1.5f, 1f, t); // 从1.5缩回1
+            transform.localScale = Vector3.one * scale;
+
+            // 移动
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+
             if (numberSpawnCounter <= 0)
             {
                 DamageNumberController.instance.RecicleNumberToPool(this);
             }
-
         }
-
-        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
-
     }
 
-    public void SetUp (int damageDisplay)
-    {
-        numberSpawnCounter = numberSpawnTime;
-
-        damageText.text = damageDisplay.ToString();
-    }
 }
--- GameSceneScripts/Weapon/Weapon.cs
+++ GameSceneScripts/Weapon/Weapon.cs
@@ -4,42 +4,13 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    //Lista de estados que tiene la arma en cada de su nivel
-    //public List<WeaponStatus> stats;
-    //[HideInInspector] public int weaponLevel;
 
-    //[HideInInspector]public bool statsUpdated;
-
-    public Sprite icon;
-
-
-    //new attribute
     public int weaponID;
 
     public float might, coolDown, area, speed, duration;
     public int amount;
 
-    /*
-    public void LevelUpWeapon()
-    {
-        if (weaponLevel < stats.Count - 1)
-        {
-            weaponLevel++;
 
-            statsUpdated = true;
-
-
-            //Al alcanzar el m¨¢ximo nivel del arma, lo elimina
-            //de la lista de assignedweapon y lo reemplaza al fullylevelledWeapon list
-            if (weaponLevel >= stats.Count - 1)
-            {
-                PlayerController.instance.fullyLevelledWeapons.Add(this);
-                PlayerController.instance.assignedWeapons.Remove(this);
-            }
-        }
-
-    }
-    */
     public void SetAttributes(List<Attribute> attibutes)
     {
         might = attibutes[1].value;
@@ -59,15 +30,5 @@ public class Weapon : MonoBehaviour
 
 
 }
-
-/*
-[System.Serializable]
-public class WeaponStatus
-{
-    public float speed, damage, range, timeBetweenAttacks, duration;
-    public int amount;
-    public string upgradeText;
-}
-*/
 
 
